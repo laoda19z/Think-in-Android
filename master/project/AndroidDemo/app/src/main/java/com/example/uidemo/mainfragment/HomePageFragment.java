fcc79c0009c41e3ed1aa4a2042ab4f0eab481a62
DINGZHIQIANGNB <2940526258@qq.com>
2020-12-04 08:14:50 +0800

    DZQ

--- master/project/AndroidDemo/app/src/main/java/com/example/uidemo/mainfragment/HomePageFragment.java
+++ master/project/AndroidDemo/app/src/main/java/com/example/uidemo/mainfragment/HomePageFragment.java
@@ -2,24 +2,23 @@ package com.example.uidemo.mainfragment;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.uidemo.R;
 import com.example.uidemo.adapter.BannerAdapter;
-import com.example.uidemo.adapter.BannerAdapterTwo;
+import com.example.uidemo.adapter.HomePageHorizontalListViewAdapter;
 import com.example.uidemo.beans.Data;
 import com.example.uidemo.course.CourseActivity;
 import com.example.uidemo.familyactivity.activities.FamilyActivity;
+import com.example.uidemo.record.HorizontalListView;
 import com.example.uidemo.record.RecordActivity;
 import com.xuexiang.xui.widget.banner.recycler.BannerLayout;
 
@@ -33,8 +32,8 @@ public class HomePageFragment extends FragmentActivity {
     private LinearLayout btnRecord;
     private LinearLayout btnActivity;
     private LinearLayout btnCourse;
-    private BannerLayout twobannerlayout;
-    private BannerAdapterTwo bannerAdapter2;
+
+    private HorizontalListView horizontalListView;
     public int[] urls = new int[]{//轮播图片地址
             R.drawable.record1,
             R.drawable.record2,
@@ -43,28 +42,6 @@ public class HomePageFragment extends FragmentActivity {
             R.drawable.record5,
     };
 
-//    @Nullable
-//    @Override
-//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        view = inflater.inflate(R.layout.homepage_fragment,null);
-//        bannerLayout=view.findViewById(R.id.recybanner);
-//        mAdapter = new BannerAdapter(this,urls);
-//        bannerLayout.setAdapter(mAdapter);
-//
-//        twobannerlayout=view.findViewById(R.id.tworecybanner);
-//        List<Data> list=new ArrayList<>();
-//        Data data1=new Data(R.drawable.record1,"是联合国是读后感凉山大火");
-//        Data data2=new Data(R.drawable.record2,"大圣归来函数的观看");
-//        Data data3=new Data(R.drawable.record3,"大圣归来函数的观看");
-//        Data data4=new Data(R.drawable.record3,"大圣归来函数的观看");
-//        list.add(data1);
-//        list.add(data2);
-//        list.add(data3);
-//        list.add(data4);
-//        bannerAdapter2=new BannerAdapterTwo(context,list,R.layout.banner2);
-//        twobannerlayout.setAdapter(bannerAdapter2);
-//        return view;
-//    }
 
     @Nullable
     @Override
@@ -74,10 +51,12 @@ public class HomePageFragment extends FragmentActivity {
         btnRecord = view.findViewById(R.id.homepage_record);
         btnActivity = view.findViewById(R.id.homepage_activity);
         btnCourse = view.findViewById(R.id.homepage_course);
+
         mAdapter = new BannerAdapter(context,urls);
         bannerLayout.setAdapter(mAdapter);
 
-        twobannerlayout=view.findViewById(R.id.tworecybanner);
+
+        horizontalListView=view.findViewById(R.id.hor);
         List<Data> list=new ArrayList<>();
         Data data1=new Data(R.drawable.record1,"是联合国是读后感凉山大火");
         Data data2=new Data(R.drawable.record2,"大圣归来函数的观看");
@@ -87,8 +66,16 @@ public class HomePageFragment extends FragmentActivity {
         list.add(data2);
         list.add(data3);
         list.add(data4);
-        bannerAdapter2=new BannerAdapterTwo(context,list,R.layout.banner2);
-        twobannerlayout.setAdapter(bannerAdapter2);
+        HomePageHorizontalListViewAdapter homePageHorizontalListViewAdapter =new HomePageHorizontalListViewAdapter(context,list, R.layout.banner2);
+        horizontalListView.setAdapter(homePageHorizontalListViewAdapter);
+        horizontalListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //点击listview中
+
+            }
+        });
+
         btnRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
